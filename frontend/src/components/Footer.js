4b7d62c22aff88c1c579195ac66384936000baab
ashu24998 <ashutosh24998@gmail.com>
2023-05-27 10:02:14 +0530

    Changes in footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,48 +2,54 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div class="container my-5">
+    <div className="container my-5">
       <footer
-        class="text-center text-lg-start"
-        style="background-color: #db6930;"
+        className="text-center text-lg-start"
+        style={{ backgroundColor: "#db6930" }}
+        // style="background-color: #db6930;"
       >
-        <div class="container d-flex justify-content-center py-5">
+        <div className="container d-flex justify-content-center py-5">
           <button
             type="button"
-            class="btn btn-primary btn-lg btn-floating mx-2"
-            style="background-color: #54456b;"
+            className="btn btn-primary btn-lg btn-floating mx-2"
+            style={{ backgroundColor: "#54456b" }}
+            // style="background-color: #54456b;"
           >
-            <i class="fab fa-facebook-f"></i>
+            <i className="fab fa-facebook-f"></i>
           </button>
           <button
             type="button"
-            class="btn btn-primary btn-lg btn-floating mx-2"
-            style="background-color: #54456b;"
+            className="btn btn-primary btn-lg btn-floating mx-2"
+            style={{ backgroundColor: "#54456b" }}
+            // style="background-color: #54456b;"
           >
-            <i class="fab fa-youtube"></i>
+            <i className="fab fa-youtube"></i>
           </button>
           <button
             type="button"
-            class="btn btn-primary btn-lg btn-floating mx-2"
-            style="background-color: #54456b;"
+            className="btn btn-primary btn-lg btn-floating mx-2"
+            style={{ backgroundColor: "#54456b" }}
+            // style="background-color: #54456b;"
           >
-            <i class="fab fa-instagram"></i>
+            <i className="fab fa-instagram"></i>
           </button>
           <button
             type="button"
-            class="btn btn-primary btn-lg btn-floating mx-2"
-            style="background-color: #54456b;"
+            className="btn btn-primary btn-lg btn-floating mx-2"
+            style={{ backgroundColor: "#54456b" }}
+            // style="background-color: #54456b;"
           >
-            <i class="fab fa-twitter"></i>
+            <i className="fab fa-twitter"></i>
           </button>
         </div>
         <div
-          class="text-center text-white p-3"
-          style="background-color: rgba(0, 0, 0, 0.2);"
+          className="text-center text-white p-3"
+          style={{ backgroundColor: "grey" }}
+          //   style="background-color: rgba(0, 0, 0, 0.2);"
         >
-          © 2020 Copyright:
-          <a class="text-white" href="https://mdbootstrap.com/">
-            MDBootstrap.com
+          © 2023 Copyright:
+          <a className="text-white" href="https://safehealthcare.netlify.app">
+            Created by Ashutosh Choubey
           </a>
         </div>
       </footer>
